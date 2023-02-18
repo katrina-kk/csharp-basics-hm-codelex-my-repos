a7aba808fd351b66654b4d101514c3bb8abf6120
katrina-kk <katrinakraukle.kk@gmail.com>
2023-02-18 20:49:27 +0200

    Exercise 6

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise6/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Exercise6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+           
+            Console.WriteLine("Enter how many numbers you would like to sum up? ");
+            int userInputTotal = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < userInputTotal; i++)
+            {
+                Console.WriteLine("Enter a number: ");
+                int userInputNumber = Convert.ToInt32(Console.ReadLine());
+                sum += userInputNumber;
+            }
+
+            Console.WriteLine($"The sum of your numbers is: {sum}.");
+            Console.ReadKey();             
+
+        }
+    }
+}

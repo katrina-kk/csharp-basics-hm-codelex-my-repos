4457dd9fc9dc122bd1b9c4f191455c77731b070e
katrina-kk <katrinakraukle.kk@gmail.com>
2023-02-18 21:15:07 +0200

    Exercise 7

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise7/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercise7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a sentence. For fun try to use some upper case letters: ");
+            string userInput = Console.ReadLine();
+
+            int countUpperCase = 0;
+            int i;
+
+            for (i = 0; i < userInput.Length; i++)
+            {
+                if (char.IsUpper(userInput[i]))
+                {
+                    countUpperCase++;
+                }
+                else
+                {
+                    Console.Write("");
+                }
+            }   
+            
+            Console.WriteLine($"You have  written a sentence using {countUpperCase} upper case letters.");
+       }
+    }
+ }

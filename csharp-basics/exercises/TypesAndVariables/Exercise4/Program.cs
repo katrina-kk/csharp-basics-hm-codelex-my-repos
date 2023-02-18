6a1a8839c5f0676ce4ebaa6c3db47b8981da85c5
katrina-kk <katrinakraukle.kk@gmail.com>
2023-02-18 17:56:26 +0200

    Ex4

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise4/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string fullName = "Katrina Kraukle";
+            int year = 1993;
+
+            Console.WriteLine($"My name is {fullName} and I was born in {year}.");
+            Console.ReadKey();
+        }
+    }
+}

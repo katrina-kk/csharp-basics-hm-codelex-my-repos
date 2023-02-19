64ed15cdb2f95fc129790e0e6c8ded96c0ef71e8
katrina-kk <katrinakraukle.kk@gmail.com>
2023-02-19 15:12:49 +0200

    Exercise 8 minutes in years and minutes

--- csharp-basics/exercises/TypesAndVariables/Exercise8/Program.cs
+++ csharp-basics/exercises/TypesAndVariables/Exercise8/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Insert how many minutes you would like to convert?: ");
+            int inputMinutes = Convert.ToInt32(Console.ReadLine());
+
+            int minutesInYear = 365 * 24 * 60;
+            int minutesInDay = 24 * 60;
+            int year = inputMinutes / minutesInYear;
+            int remainingMinutes = inputMinutes % minutesInYear;
+            int day = remainingMinutes / minutesInDay;
+
+            Console.WriteLine($"Your chosen {inputMinutes} is {year} years and {day} days");
+            Console.ReadKey();
         }
     }
 }

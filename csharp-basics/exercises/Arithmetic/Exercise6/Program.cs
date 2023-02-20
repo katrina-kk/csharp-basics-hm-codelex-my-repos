5ade3b604bdfbb933302deeaa2214d8845a99df6
katrina-kk <katrinakraukle.kk@gmail.com>
2023-02-20 23:19:34 +0200

    Coza Loza

--- csharp-basics/exercises/Arithmetic/Exercise6/Program.cs
+++ csharp-basics/exercises/Arithmetic/Exercise6/Program.cs
@@ -1,20 +1,59 @@
 ﻿namespace Exercise6
 {
-    internal class Program
+    internal class CozaLozaWoza
     {
         static void Main(string[] args)
         {
-//            Write a program called CozaLozaWoza which prints the numbers 1 to 110, 11 numbers per line.
-//                The program shall print "Coza" in place of the numbers which are multiples of 3, 
-//                "Loza" for multiples of 5,
-//                "Woza" for multiples of 7,
-//                "CozaLoza" for multiples of 3 and 5, and so on.The output shall look like:
+            //            Write a program called CozaLozaWoza which prints the numbers 1 to 110, 11 numbers per line.
+            //                The program shall print "Coza" in place of the numbers which are multiples of 3, 
+            //                "Loza" for multiples of 5,
+            //                "Woza" for multiples of 7,
+            //                "CozaLoza" for multiples of 3 and 5, and so on.The output shall look like:
 
-//1 2 Coza 4 Loza Coza Woza 8 Coza Loza 11
-//Coza 13 Woza CozaLoza 16 17 Coza 19 Loza CozaWoza 22
-//23 Coza Loza 26 Coza Woza 29 CozaLoza 31 32 Coza
+            //1 2 Coza 4 Loza Coza Woza 8 Coza Loza 11
+            //Coza 13 Woza CozaLoza 16 17 Coza 19 Loza CozaWoza 22
+            //23 Coza Loza 26 Coza Woza 29 CozaLoza 31 32 Coza
+            
 
-            Console.WriteLine("Hello, World!");
+            
+            for (int i = 1; i <= 111; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.Write("Coza ");
+                }
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("CozaLoza ");
+                }
+              
+                else if (i % 5 == 0)
+                {
+                    Console.Write("Loza ");
+                }
+                else if (i % 7 == 0)
+                {
+                    Console.Write("Woza ");
+                }
+                else
+                {
+                    Console.Write($"{i} ");
+                }
+                if (i % 11 == 0)
+                {
+                    Console.WriteLine("");
+                }
+
+
+            }
+            Console.ReadKey();
+
         }
-    }
-}
+
+        }
+        }
+
+       
+    
+
+    

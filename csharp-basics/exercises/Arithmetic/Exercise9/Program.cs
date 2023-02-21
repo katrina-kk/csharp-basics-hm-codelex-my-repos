74424e3b226191a6cbdb6b54029d6062bc426947
katrina-kk <katrinakraukle.kk@gmail.com>
2023-02-21 16:10:56 +0200

    Difficult to wrap my mind around how to convert BMI in Ex9

--- csharp-basics/exercises/Arithmetic/Exercise9/Program.cs
+++ csharp-basics/exercises/Arithmetic/Exercise9/Program.cs
@@ -4,16 +4,43 @@
     {
         static void Main(string[] args)
         {
-//            Write a program that calculates and displays a person's body mass index (BMI).
-//                A person's BMI is calculated with the following formula: BMI = weight x 703 / height ^ 2.
-//                Where weight is measured in pounds and height is measured in inches.Display a message indicating whether the person has optimal weight, 
-//                is underweight, or is overweight.A sedentary person's weight is considered optimal if his or her BMI is between 18.5 and 25. 
-//                If the BMI is less than 18.5, the person is considered underweight. 
-//                If the BMI value is greater than 25, the person is considered overweight.
+            //            Write a program that calculates and displays a person's body mass index (BMI).
+            //                A person's BMI is calculated with the following formula: BMI = weight x 703 / height ^ 2.
+            //                Where weight is measured in pounds and height is measured in inches.Display a message indicating whether the person has optimal weight, 
+            //                is underweight, or is overweight.A sedentary person's weight is considered optimal if his or her BMI is between 18.5 and 25. 
+            //                If the BMI is less than 18.5, the person is considered underweight. 
+            //                If the BMI value is greater than 25, the person is considered overweight.
 
-//Your program must accept metric units.
+            //Your program must accept metric units.
 
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your weight?");
+            double weight = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is your height?");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            double poundsToKG = weight * 0.45;
+            double inchesToCM = height * 2.54;
+            double bmiToMetric = ((weight * 0.45) * 703) / Math.Pow(inchesToCM, 2));
+            
+
+            if(bmiToMetric >= 25)
+            {
+                Console.WriteLine($"You are owerweight. Your BMI is {Math.Round(bmiToMetric, 2)}");
+            }
+            else if(bmiToMetric <= 18.5)
+            {
+               
+                Console.WriteLine($"You are underweight.Your BMI is {Math.Round(bmiToMetric, 2)}");
+            }
+            else if(bmiToMetric >= 18.5 && bmiToMetric >= 25)
+            {
+                
+                Console.WriteLine($"Your weight is optimal. Your BMI is {Math.Round(bmiToMetric, 2)}");
+            }
+            Console.ReadKey();
+
+           
+            
         }
     }
 }

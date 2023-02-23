eabbe056590bb6125d076c914d9999e6b4caa98e
katrina-kk <katrinakraukle.kk@gmail.com>
2023-02-23 14:50:31 +0200

    Exercise 1 and 2 done

--- csharp-basics/exercises/FlowOfControl/FlowControl/TenBillion/Program.cs
+++ csharp-basics/exercises/FlowOfControl/FlowControl/TenBillion/Program.cs
@@ -4,18 +4,19 @@ namespace TenBillion
 {
     class Program
     {
-        //TODO Write a C# program that reads an positive integer and count the number of digits the number (less than ten billion) has.
+        //TODO Write a C# program that reads an positive integer and count the number of digits
+        //the number (less than ten billion) has.
         static void Main(string[] args)
         {
             Console.WriteLine("Input an integer number less than ten billion: ");
 
             var input = Console.ReadLine();
 
-            /*
+            
             todo - check if number
             if (?) 
             {
-                //todo - check if n is less than zero
+                todo - check if n is less than zero
                 if (?) 
                 {
                     n *= -1;
@@ -73,7 +74,7 @@ namespace TenBillion
             {
                 Console.WriteLine("The number is not a long");
             }
-            */
+            Console.ReadKey();
         }
     }
 }

349b27f80f081f0cb7ce8af0996fb087869e19c9
katrina-kk <katrinakraukle.kk@gmail.com>
2023-02-23 15:25:19 +0200

    Exercise 3 and 4 done

---
+++ csharp-basics/exercises/FlowOfControl/FlowControl/Exercise4/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number from 1 - 7.");
+            var userInput = Int32.Parse(Console.ReadLine());
+
+            switch(userInput)
+            {
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Error. You entered a number out of range.");
+                    break;
+            }
+        }
+    }
+}
--- csharp-basics/exercises/FlowOfControl/FlowControl/TenBillion/Program.cs
+++ csharp-basics/exercises/FlowOfControl/FlowControl/TenBillion/Program.cs
@@ -10,63 +10,60 @@ namespace TenBillion
         {
             Console.WriteLine("Input an integer number less than ten billion: ");
 
-            var input = Console.ReadLine();
+            var userInput = Int64.Parse(Console.ReadLine());
 
+            bool tooLong = true;
             
-            todo - check if number
-            if (?) 
-            {
-                todo - check if n is less than zero
-                if (?) 
+            if (tooLong) 
+            {               
+                if (userInput < 0) 
                 {
-                    n *= -1;
+                    userInput *= -1;
                 }
-
-                fixme
-                if (?) 
+               
+                if (userInput >= 10000000000) 
                 {
                     Console.WriteLine("Number is greater or equals 10,000,000,000!");
                 } 
                 else 
                 {
                     int digits = 1;
-                    if (?) 
+                    if (userInput >= 10 && userInput < 100) 
                     {
                         digits = 2;
                     } 
-                    else if (?) 
+                    else if (userInput >= 100 && userInput < 1000) 
                     {
                         digits = 3;
                     } 
-                    else if (?) 
+                    else if (userInput >= 1000 && userInput < 10000) 
                     {
                         digits = 4;
                     } 
-                    else if (?) 
+                    else if (userInput >= 10000 && userInput < 100000) 
                     {
                         digits = 5;
                     } 
-                    else if (?) 
+                    else if (userInput >= 100000 && userInput < 1000000) 
                     {
                         digits = 6;
                     } 
-                    else if (?) 
+                    else if (userInput >= 1000000 && userInput < 10000000) 
                     {
                         digits = 7;
                     } 
-                    else if (?) 
+                    else if (userInput >= 10000000 && userInput < 100000000) 
                     {
                         digits = 8;
                     } 
-                    else if (?) 
+                    else if (userInput >= 100000000 && userInput < 1000000000) 
                     {
                         digits = 9;
                     } 
-                    else if (?) 
+                    else if (userInput >= 1000000000 && userInput < 10000000000) 
                     {
                         digits = 10;
                     }
-
                     Console.WriteLine("Number of digits in the number: " + digits);
                 }
             } 

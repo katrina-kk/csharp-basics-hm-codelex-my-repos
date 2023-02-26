a0c906514ec5f787e2976f78103d73b3abcb91e8
katrina-kk <katrinakraukle.kk@gmail.com>
2023-02-26 21:46:33 +0200

    Exercise6 done

--- csharp-basics/exercises/Arrays/Arrays/Exercise6/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise6/Program.cs
@@ -3,18 +3,32 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-//            Write a program that creates an array of ten integers. It should put ten random numbers from 1 to 100 in the array. It should copy all the elements of that array into another array of the same size.
+        {         
+            int[] values = new int[10];
+            int[] values2 = new int[10];
+            Random randomNum = new Random();
 
-//Then display the contents of both arrays.To get the output to look like this, you'll need a several for loops.
+            for (int i = 1; i < values.Length; i++)
+            {
+                values[i] = randomNum.Next(1, 100);
+            }                      
 
-//Create an array of ten integers
-//Fill the array with ten random numbers(1 - 100)
-//Copy the array into another array of the same capacity
-//Change the last value in the first array to a - 7
-//Display the contents of both arrays
-//Array 1: 45 87 39 32 93 86 12 44 75 - 7
-//Array 2: 45 87 39 32 93 86 12 44 75 50
+            values.CopyTo(values2, 0);
+            values.SetValue(-7, 9);
 
+            foreach (int item in values)
+            {
+                Console.Write(item + ", ");
+            }
+
+            Console.WriteLine();
+
+            foreach (int item in values2)
+            {
+                Console.Write(item + ", ");
+            }
+                
+            Console.ReadKey();        
+        }
     }
 }

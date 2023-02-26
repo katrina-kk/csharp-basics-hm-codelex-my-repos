88371038586338b0927b1194147bd459fe329d42
katrina-kk <katrinakraukle.kk@gmail.com>
2023-02-26 23:31:02 +0200

    Struggling with Exercise 9 and 10

--- csharp-basics/exercises/Arrays/Arrays/Exercise10/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise10/Program.cs
@@ -6,9 +6,14 @@
         {
             //Create a function that takes an array of positive and negative numbers.
             //Return an array where the first element is the count of positive numbers and the second element is the sum of negative numbers.
-//            If given an empty array, return an empty array: []
-//            Cast sum to int, don't mind the remaining decimal places.
-//0 is not positive.
+            //            If given an empty array, return an empty array: []
+            //            Cast sum to int, don't mind the remaining decimal places.
+            //0 is not positive.
+
+            CountPosSumNeg([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]);
         }
+        static int CountPosSumNeg(int[] input)
+        {
+            
     }
 }
--- csharp-basics/exercises/Arrays/Arrays/Exercise9/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise9/Program.cs
@@ -1,12 +1,49 @@
-﻿namespace Exercise9
+﻿using System.Text.RegularExpressions;
+
+namespace Exercise9
 {
     internal class Program
     {
-        static void Main(string[] args)
+       public static void Main(string[] args)
         {
             //Create a function that takes an array of names and returns an array where only the first letter of each name is capitalized.
-//            Don't change the order of the original array.
-//Notice in the second example above, "MABELLE" is returned as "Mabelle".
+            //            Don't change the order of the original array.
+            //Notice in the second example above, "MABELLE" is returned as "Mabelle".
+
+            string[] strArray = new string[] { "one", "two", "three", "four", "five", "six" }; // Creating Array of String.
+
+
+            //var myArrayLower = strArray.Select(s => s.ToLowerInvariant()).ToArray();
+
+           
+
+        //    string mostLower = strArray.Substring(0, 1).ToUpper() + strArray.Substring(1).ToLower();
+        //    // Call firstLetterCapital function for each word inside strArray array.
+        //    for (int i = 0; i < strArray.Length; i++)
+        //    {
+        //        strArray[i] = NamesCapitalizedFirst(strArray[i]);
+        //    }
+        //    //This for each loop will print each element in array of string.
+        //    foreach (string str in strArray)
+        //    {
+        //        Console.WriteLine(str);
+        //    }
+        //    Console.ReadKey();
+        //}
+        //static string NamesCapitalizedFirst(string names)
+        //{
+           
+        //    string firstLetter = names.Substring(0, 1);  //Take first letter as new string,
+        //                                               // this done with help of substring method of String class.
+        //    string firstLetterCapital = firstLetter.ToUpper();
+        //    string strExcludeFirstLetter = names.Substring(1, names.Length);
+        //    // Final string will be firstLetterCapital followed by strExceptFirstLetter.
+        //    string result = firstLetterCapital + strExcludeFirstLetter;
+        //    return result;
         }
+        
+        // This function will return new copy of str (input string) but with first letter as capital.
+        // Notice this function is static because we going to call this function from main method. and main method is static.
+        
     }
 }

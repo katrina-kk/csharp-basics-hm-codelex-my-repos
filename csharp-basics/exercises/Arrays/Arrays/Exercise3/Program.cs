e2300373e4c65e18356b7740fa5ee2270448ece3
katrina-kk <katrinakraukle.kk@gmail.com>
2023-02-26 18:44:05 +0200

    Exercise 5 done

--- csharp-basics/exercises/Arrays/Arrays/Exercise3/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise3/Program.cs
@@ -4,8 +4,7 @@ using System.Linq;
 namespace Exercise3
 {
     class Program
-    {
-        //TODO: Write a C# program to calculate the average value of array elements.
+    {       
         private static void Main(string[] args)
         {
             int[] numbers = {20, 30, 25, 35, -16, 60, -100};

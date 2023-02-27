8ebfb04df6ae1dc9b113f98586d548d4d57f841d
katrina-kk <katrinakraukle.kk@gmail.com>
2023-02-27 12:19:41 +0200

    Exercise 10 not sure about the solution. It seems like it works but probably there is more convenient way to do this.

--- csharp-basics/exercises/Arrays/Arrays/Exercise10/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise10/Program.cs
@@ -12,8 +12,10 @@
 
             CountPosSumNeg([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]);
         }
-        static int CountPosSumNeg(int[] input)
+        static int[] CountPosSumNeg(int[] input)
         {
+
+        }
             
     }
 }

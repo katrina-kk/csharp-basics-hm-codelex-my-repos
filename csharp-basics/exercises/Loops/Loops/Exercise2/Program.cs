e316be1469ca2bf541da8f5a53489e9207f54174
katrina-kk <katrinakraukle.kk@gmail.com>
2023-03-01 00:49:10 +0200

    Update exercise2

--- csharp-basics/exercises/Loops/Loops/Exercise2/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Exercise2/Program.cs
@@ -9,14 +9,12 @@ namespace Exercise2
             int i;
             Console.WriteLine("Input number to multiply with itself: ");
             var number = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Input number how many times to multiply that number: ");
-            var power = Convert.ToInt32(Console.ReadLine());
-           
+       
             var answer = 1;
 
             for ( i = 0; i < number; i++)
             {
-                answer = answer * power;
+                answer = answer * number;
             }
 
            Console.WriteLine(answer);

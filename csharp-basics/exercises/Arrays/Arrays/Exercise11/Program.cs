0281161b24b9f23227b514e7e8424ca469fcacfd
katrina-kk <katrinakraukle.kk@gmail.com>
2023-02-28 12:38:53 +0200

    Ex11

--- csharp-basics/exercises/Arrays/Arrays/Exercise11/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise11/Program.cs
@@ -1,45 +1,42 @@
 ﻿namespace Exercise11
 {
     internal class Program
-    {
+    {     
         static void Main(string[] args)
         {
-            //            You need to find the word "Nemo", and return a string like this: "I found Nemo at [the order of the word you find Nemo]!".
-            //                If you can't find Nemo, return "I can't find Nemo: (".
-            //                FindNemo("I am finding Nemo !") ➞ "I found Nemo at 4!"
+            Console.WriteLine("Do you want to find Nemo? Write one sentence using word Nemo in it or choose not to: ");
+            string findNemo = Console.ReadLine();
 
-            //FindNemo("Nemo is me") ➞ "I found Nemo at 1!"
+            int nemoIndex = GetIndexNemo(findNemo);
 
-            //FindNemo("I Nemo am") ➞ "I found Nemo at 2!"
-            //Hints:
-            //            ! , ? . are always separated from the last word.
-            //Nemo will always look like Nemo, and not NeMo or other capital variations.
-            //Nemo's, or anything that says Nemo with something behind it, doesn't count as Finding Nemo.
-            //If there are multiple Nemo's in the sentence, only return the first one.
+            if (nemoIndex != 0)
+            {
+                Console.WriteLine($"I found Nemo at {nemoIndex}!");
+            }
+            else
+            {
+                Console.WriteLine("I can't find Nemo");
+            }
 
-            string input1 = "Nemo is me";
-            string input2 = "I Nemo am";
-
-            string[] output1 = input1.Select(x => x.ToString()).ToArray();
-
-            //string[] output1 = new[] { input1 };
-            //string[] output2 = new[] { input2 };
-
-            //string searchNemo = "Nemo";
-            int index = Array.FindIndex(output1, x => x.Contains("Nemo"));
-            //Console.WriteLine("I found nemo at" + index);
-
-            string item = "Nemo";
-
-            string index = Array.FindIndex(item);
-            
-
-
-            //int index2 = Array.IndexOf(output2, searchNemo);
-            //Console.WriteLine("Index of '" + searchNemo + "' : " + index);
+            Console.ReadLine();       
         }
-    }
+
+       static int GetIndexNemo(string findNemo)
+        {
+            string[] words = findNemo.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i] == "Nemo")
+                {
+                    return (i + 1);
+                }
+            }
+            
+            return 0;
+        }
+    }   
 }
 
 
-   
+

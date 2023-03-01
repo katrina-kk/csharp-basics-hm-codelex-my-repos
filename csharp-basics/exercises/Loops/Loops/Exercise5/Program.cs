312d9ead6aa91dac2ca988edc034d9f3577dd073
katrina-kk <katrinakraukle.kk@gmail.com>
2023-03-01 12:11:32 +0200

    exercise6

--- csharp-basics/exercises/Loops/Loops/Exercise5/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Exercise5/Program.cs
@@ -4,14 +4,22 @@
     {
         static void Main(string[] args)
         {
-//            Write a program that asks the user to enter two words.The program then prints out both words on one line.The words will be separated by enough dots so that the total line length is 30:
+            Console.WriteLine("Enter word 1: ");
+            var word1 = Console.ReadLine();
+            Console.WriteLine("Enter word 2: ");
+            var word2 = Console.ReadLine();
+            
+            var concat = word1 + word2;
+            int stringLength = concat.Length;
+            var dotsLength = 30 - stringLength;
+            var dots = ".";
 
-//Enter first word:
-//            turtle
-//            Enter second word
-//153
-//turtle....................153
-//This could be used as part of an index for a book. To print out the dots, use Console.WriteLine(".") inside a loop body.
+            Console.Write(word1);
+            for(int i = 0; i < dotsLength; i++)
+            {                
+                Console.Write(dots);
+            }
+            Console.WriteLine(word2);
         }
     }
 }

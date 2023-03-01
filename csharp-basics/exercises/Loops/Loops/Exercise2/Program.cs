d493456c72d1a0c890e4161ac6cd8734ae90fd8b
katrina-kk <katrinakraukle.kk@gmail.com>
2023-03-01 10:38:44 +0200

    Exercise3

--- csharp-basics/exercises/Loops/Loops/Exercise2/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Exercise2/Program.cs
@@ -7,9 +7,8 @@ namespace Exercise2
         static void Main(string[] args)
         {           
             int i;
-            Console.WriteLine("Input number to multiply with itself: ");
-            var number = Convert.ToInt32(Console.ReadLine());
-       
+            Console.WriteLine("Input number of terms : ");
+            var number = Convert.ToInt32(Console.ReadLine());       
             var answer = 1;
 
             for ( i = 0; i < number; i++)
--- csharp-basics/exercises/Loops/Loops/Exercise3/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Exercise3/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            //Write a program that asks which number position out of 20 random numbers you want to know.
+            int[] randomNumbers = new int[20];
+            Random randNum = new Random();
+
+            for (int i = 0; i < randomNumbers.Length; i++)
+            {
+                randomNumbers[i] = randNum.Next(0, 20);
+            }
+
+            foreach(int i in randomNumbers)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+            Console.WriteLine("Which number position out of these 20 random you want to know : ");
+            var position = int.Parse(Console.ReadLine());
+
+            int index = Array.IndexOf(randomNumbers, position); 
+            Console.WriteLine($"This is the position of your chosen number: {index.ToString()}");
         }
     }
 }

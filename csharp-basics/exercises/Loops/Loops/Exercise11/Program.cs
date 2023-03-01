fd676fc60f0db26c6058bedc6af31cf3d322f61f
katrina-kk <katrinakraukle.kk@gmail.com>
2023-03-01 16:04:42 +0200

    Piglet

--- csharp-basics/exercises/Loops/Loops/Exercise11/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Exercise11/Program.cs
@@ -4,12 +4,25 @@
     {
         static void Main(string[] args)
         {
-//            Given a string, create a function to reverse the case. All lower-cased letters should be upper-cased, and vice versa.
+            //            Given a string, create a function to reverse the case. All lower-cased letters should be upper-cased, and vice versa.
 
-//ReverseCase("Happy Birthday") ➞ "hAPPY bIRTHDAY"
-//ReverseCase("MANY THANKS") ➞ "many thanks"
-//ReverseCase("sPoNtAnEoUs") ➞ "SpOnTaNeOuS"
+            //ReverseCase("Happy Birthday") ➞ "hAPPY bIRTHDAY"
+            //ReverseCase("MANY THANKS") ➞ "many thanks"
+            //ReverseCase("sPoNtAnEoUs") ➞ "SpOnTaNeOuS"
 
+            Console.WriteLine("Insert a string: ");
+            var userInput = Console.ReadLine();
+
+            char[] charString = userInput.ToCharArray();
+
+           for(int i = 0; i < charString.Length; i++)
+            {
+                if (Char.IsLower[])
+                {
+
+                }
+            }
+            
         }
     }
 }
---
+++ csharp-basics/exercises/Loops/Loops/Exercise7/Piglet.cs
@@ -0,0 +1,67 @@
+﻿namespace Exercise7
+{
+    internal class Piglet
+    {
+        static void Main(string[] args)
+        {
+            int playerRandomNum;
+            int result = 0;
+            Random random = new Random();
+
+            Console.WriteLine("Welcome to game Piglet. Press enter if you want to start the game");
+            Console.ReadKey();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Press any key to roll the dice. If you want to end the game press 9: ");
+
+                var userInput = Convert.ToInt32(Console.ReadLine());
+
+                playerRandomNum = random.Next(1, 6);
+                if (userInput == 9)
+                {
+                    Console.WriteLine($"Your score is {result}. Thanks for the game");
+                    break;
+                }
+                else if (playerRandomNum == 1)
+                {
+                    Console.WriteLine("You lost. You got 0 point");
+                    break;
+                }
+                else if (playerRandomNum == 2)
+                {
+                    result += playerRandomNum;
+                    Console.WriteLine($"Your result is {result}");
+                    continue;
+                }
+                else if (playerRandomNum == 3)
+                {
+                    result += playerRandomNum;
+                    Console.WriteLine($"Your result is {result}");
+                    continue;
+
+                }
+                else if (playerRandomNum == 4)
+                {
+                    result += playerRandomNum;
+                    Console.WriteLine($"Your result is {result}");
+                    continue;
+
+                }
+                else if (playerRandomNum == 5)
+                {
+                    result += playerRandomNum;
+                    Console.WriteLine($"Your result is {result}");
+                    continue;
+
+                }
+                else if (playerRandomNum == 6)
+                {
+                    result += playerRandomNum;
+                    Console.WriteLine($"Your result is {result}");
+                    continue;
+                }
+            }
+        }
+    }
+}

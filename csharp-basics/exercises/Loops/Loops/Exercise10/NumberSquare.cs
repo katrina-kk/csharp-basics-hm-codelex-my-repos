5dd66805bac77268ececb3b170426b8ef195c138
katrina-kk <katrinakraukle.kk@gmail.com>
2023-03-01 17:33:43 +0200

    NumberSquare

---
+++ csharp-basics/exercises/Loops/Loops/Exercise10/NumberSquare.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercise10
+{
+    internal class NumberSquare
+    {
+        static void Main(string[] args)
+        {           
+            Console.WriteLine("Insert min number you want your patter to start with");
+            var userInputMin = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insert max number you want your patter to end with");
+            var userInputMax = Convert.ToInt32(Console.ReadLine());          
+
+            for(int i = userInputMin; i <= userInputMax; i++)
+            {
+                for (int j = i; j <= userInputMax; j++)
+                {
+                    Console.Write(j);                   
+                }
+                for (int k = userInputMin; k < i; k++)
+                {
+                    Console.Write(k);
+                }
+                Console.WriteLine();              
+            }
+        }
+    }
+}
